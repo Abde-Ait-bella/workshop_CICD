09ad49a68f019e5ea35cb045edefc6e93c4715d2
Abdessamad Ait-bella <abdessamadaitbella1998@gmail.com>
2024-12-26 01:23:28 +0100

    test github-action

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => '1388ac2fc50d378e5b0b8c4279971f9e055a7eab',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => '1388ac2fc50d378e5b0b8c4279971f9e055a7eab',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
