fc541190ae90d89b65403938145d6d9ee8d0b78f
Abdessamad Ait-bella <abdessamadaitbella1998@gmail.com>
2025-02-12 19:12:23 +0100

    ...

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -3,7 +3,6 @@ use PHPUnit\Framework\TestCase;
 
 class CounterTest extends TestCase {
     public function testCounterIncrements() {
-        
         $file = './counter.txt';
 
         // Simuler une lecture et incrémentation
